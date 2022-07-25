d21511e42d5267d22ed63f2f5fd5ceb9baac07f7
NailtonSSantos <nltsilva8@gmail.com>
2022-07-25 19:34:07 -0400

    Consfigurado as páginas home e cadastro

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Requisitos: Visual Studeo, Gitbash, Mysql
 
 PASSOS:
 
-- No terminal dá o comando npm install express
+- Instalar o Nodemon, no terminal dá o comando npm install express
 
-- Digita o comendo npm install --save-dev nodemon
+- Instalar o Nodemon, no terminal digita o comando npm install --save-dev nodemon
+
+- Instalar o EJS, no Terminal digita npm install ejs
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require('express')
-const http = require('http')
-const fs = require('fs')
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const server = http.createServer(function(req, res){
-    console.log(req.url)
+//DEFINI O TEMPLATE ENGINE
+app.set('view engine', 'ejs')
 
-    fs.readFile('../src/views/index.html', function(error, content){
-        res.end(content)
-    })
+
+//DEFINI OS ARQUIVOS ESTÁTICOS
+//app.use(express.static(path.join(__dirname, 'views')));
+
+
+//DEFINI OS ARQUIVOS PÚBLICOS
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//ROTAS
+app.get('/', (req, res) => {
+    res.render('index');
 })
 
-server.listen(3000)
-console.log('Servidor escultando na porta 3000')
+app.get('/cadastroAgricultores', (req, res) => {
+    res.render('cadastroAgricultores');
+})
+
+
+//404 ERROR
+app.use((req, res) => { //middleware
+    res.send('ERROR 404, Página não encontrada');
+})
+
+
+//EXECUTA O SERVIDOR
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
